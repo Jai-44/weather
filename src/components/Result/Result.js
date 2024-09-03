102d57577e3361a4cce20077ab3cf52f26cd19e6
Jai Shankar Kasnina <kasinajaishankar05@gmail.com>
2024-09-03 13:11:16 +0530

    Initial Commit

---
+++ src/components/Result/Result.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Result() {
+  return (
+    <div>
+      <p>Result Component</p>
+    </div>
+  );
+}
+
+export default Result;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Search.css";
+
+function Search() {
+  return (
+    <div>
+      <p>Search Component</p>
+    </div>
+  );
+}
+
+export default Search;
