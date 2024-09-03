0d3da3181935d6c43663604c6b01e21315a19be7
Jai Shankar Kasnina <kasinajaishankar05@gmail.com>
2024-09-03 20:26:51 +0530

    Update UI for Search And Result

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "./Result.css";
 
 function Result() {
   return (
-    <div>
-      <p>Result Component</p>
+    <div className="weatherResult">
+      <div>
+        <p>City Name</p>
+      </div>
+      <div className="Animation">
+        <p>Animation</p>
+      </div>
+      <div className="temparature">
+        <p>Temparature</p>
+      </div>
+      <div className="description">Description</div>
+      <div className="weatherParameters">
+        <div className="humidity">
+          <p>Humidity</p>
+          <p>Values</p>
+        </div>
+        <div className="preciptation">
+          <p>precipitation</p>
+          <p>Values</p>
+        </div>
+        <div className="uvIndex">
+          <p>uvIndex</p>
+          <p>Valuue</p>
+        </div>
+      </div>
     </div>
   );
 }
